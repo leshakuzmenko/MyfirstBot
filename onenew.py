15511ced6a38eac4f1160da7f3d8352e68bbc199
Alexey Kuzmenko <leshakuzmenko95@gmail.com>
2025-02-26 14:40:09 +0300

    First bot program

---
+++ onenew.py
@@ -0,0 +1,30 @@
+import requests
+import time
+
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6512132416:AAG9kxa5ZFP6-7ZxX8OsIEvKz8WGcHoUSiM'
+# TEXT = 'Ура! Классный апдейт!'
+MAX_COUNTER = 100
+
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    print(updates)
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            TEXT = result['message']['text']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
